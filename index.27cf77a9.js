9d32da589e0dba446a5cdc37e70fa7867b1f5546
Jacob <jacobxperez@gmx.com>
2023-02-26 21:39:36 -0700

    Update

--- index.27cf77a9.js
+++
@@ -1,7 +0,0 @@
-/* @license
- * Rams <https://github.com/jacobxperez/rams/>
- * Copyright (C) 2023 Jacob Perez <jacobxperez@gmx.com>
- * Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-document.addEventListener("DOMContentLoaded",(()=>{(()=>{const t=document.querySelectorAll("[data-toggle]");t.forEach((function(t){t.addEventListener("click",(e=>{t.hasAttribute("data-state","active")?t.removeAttribute("data-state"):t.setAttribute("data-state","active"),e.stopPropagation()}))})),document.addEventListener("click",(e=>{t.forEach((t=>function(t,e,a){a.target!==t&&t.matches(e)&&t.removeAttribute("data-state")}(t,'[data-toggle~="dropdown"]',e)))}))})()}));
